1e49fec56209f479ce344d56ea7326e73748efd2
manojshimpi <shimpim12@gmail.com>
2025-04-01 12:47:51 +0530

    1 April

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// index.js
+require('dotenv').config();  // Load environment variables from .env file
 
 const express = require('express');
 const mongoose = require('mongoose');
@@ -14,8 +14,14 @@ app.use(express.json());
 // MongoDB connection URL (from environment variable)
 const dbURL = process.env.DB_URL; // Local or remote DB URL
 
+// Check if DB_URL is defined
+if (!dbURL) {
+  console.error('DB_URL is not defined in the environment variables');
+  process.exit(1); // Exit the app if DB_URL is missing
+}
+
 // Connect to MongoDB
-mongoose.connect(dbURL,{
+mongoose.connect(dbURL, {
     serverSelectionTimeoutMS: 5000, // Optional: set timeout for server selection
     connectTimeoutMS: 10000, // Optional: set timeout for connection establishment
 })
